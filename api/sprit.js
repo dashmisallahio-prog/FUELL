738eb0d8306605d8f612ed17a8658fd076fa296b
dashmisallahio-prog <dashmisallahio@gmail.com>
2026-04-29 19:50:57 +0200

    Update sprit.js

--- api/sprit.js
+++ api/sprit.js
@@ -1,9 +1,14 @@
 export default async function handler(req, res) {
   const { lat, lng, fuelType } = req.query;
   const url = `https://api.e-control.at/sprit/1.0/search/gas-stations/by-address?latitude=${lat}&longitude=${lng}&fuelType=${fuelType}&includeClosed=false`;
-  
-  const data = await fetch(url).then(r => r.json());
-  
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.json(data);
-} 
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Gabim ne marrjen e te dhenave" });
+  }
+}
